9c75fcbe06f85d354729b3ce07fbb519e5a9bf0d
KARENKAMORI <kamorikaren2019@gmail.com>
2023-08-10 15:18:55 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - the main part of the program
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
