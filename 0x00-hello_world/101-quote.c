3c2c82776c390055712c640a7a651b8551eec7d2
KARENKAMORI <kamorikaren2019@gmail.com>
2023-08-10 16:04:36 -0700

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - printing a line with an error
+ * Return: 1 indicates success
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
