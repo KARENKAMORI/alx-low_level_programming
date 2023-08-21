e2690a9921aee37d969b615c2b3dec20719063d3
KARENKAMORI <kamorikaren2019@gmail.com>
2023-08-21 06:03:11 -0700

    copy string from one point to another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - copies the string being pointed at.
+ * @x : destination for copying
+ * @y: where to copy from
+ * Return: destination
+ */
+
+void *_strcpy(int *x, int y)
+{
+	int i = 0;
+	int z = 0;
+
+	while (*(y + i) != '\0')
+	{
+		i++;
+	}
+	for (; z < i; z++)
+	{
+		x[z] = y[z];
+	}
+	z[i] = '\0';
+	return (x);
+}
