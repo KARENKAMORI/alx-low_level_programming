714da52e8e1278ee3d07e278d7a7d1a4a5e3e996
KARENKAMORI <kamorikaren2019@gmail.com>
2024-03-08 01:44:50 -0800

    Doubly Linked List Implementation in C

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,21 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - Prints all dlistint_t list elements.
+ * @h: dlistint_t list head.
+ *
+ * Return: Node count.
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t num = 0;
+	size_t num = 0;
 
-    while (h)
-    {
-        num++;
-        printf("%d\n", h->n);
-        h = h->next;
-    }
+	while (h)
+	{
+		num++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
 
-    return (num);
+	return (num);
 }
