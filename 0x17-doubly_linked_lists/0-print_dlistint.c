179b59190df853981b55f2a97463f9aa9f66a1af
KARENKAMORI <kamorikaren2019@gmail.com>
2024-03-08 01:31:15 -0800

    Doubly Linked List Implementation in C

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,1 +1,15 @@
+#include "lists.h"
 
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t num = 0;
+
+    while (h)
+    {
+        num++;
+        printf("%d\n", h->n);
+        h = h->next;
+    }
+
+    return (num);
+}
