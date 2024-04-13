323c63370f373bf38fb3abce5ab56061ecb14940
KAMORIKAREN <kamorikaren2019@gmail.com>
2024-04-13 13:57:32 -0700

    a function that creates a hash table.
    Prototype: hash_table_t *hash_table_create(unsigned long int size);
    where size is the size of the array
    Returns a pointer to the newly created hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,26 +1,35 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Hash table creation function.
- * @size: array size
+ * hash_table_create - creates a hash table
  *
- * Return: Null - if an error occurs.
- * 	else - a pointer to the new hash table.
+ * @size: the size of the array
+ *
+ * Return: the newly created hash_table, or NULL on failure
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i;
-	hash_table_t *hasht
+	hash_table_t *new_ht = NULL;
+	unsigned long int index = 0;
 
-	hasht = malloc(sizeof(hash_table_t));
-	if (hasht == NULL)
-		return (NULL);
+	new_ht = malloc(sizeof(hash_table_t));
+	if (new_ht)
+	{
+		new_ht->size = size;
+		new_ht->array = malloc(sizeof(hash_node_t *) * size);
+		if (!new_ht->array)
+		{
+			free(new_ht);
+			return (NULL);
+		}
 
-	hasht->size = size;
-	hasht->array = malloc(sizeof(hash_node_t *) * size);
-	if (hasht->array == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		hasht->array[i] = NULL;
-	return (hasht);
+		while (index < size)
+		{
+			new_ht->array[index] = NULL;
+			index++;
+		}
+	}
+
+	return (new_ht);
 }
